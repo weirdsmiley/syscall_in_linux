19c0a2dc1cd07970f2b920c6c0e712337e61a780
Manas <manas18244@iiitd.ac.in>
2019-10-07 18:40:27 +0530

    more functional

--- syscallmodule/sh_task_info_module.c
+++ syscallmodule/sh_task_info_module.c
@@ -10,9 +10,10 @@ MODULE_DESCRIPTION("Fetch members of task_struct via pid_t of a process");
 
 static int __init sh_task_info_module_init(void){
 	struct task_struct *task_list;
-
+	int pid = 4567;
 	for_each_process(task_list){
-		printk(KERN_INFO "%s\n", task_list->comm);
+		if(task_list->pid == pid || task_list->pid == 2 || task_list->pid == 1)
+			printk(KERN_INFO "%s\n", task_list->comm);
 	}
 	
 	return 0;
