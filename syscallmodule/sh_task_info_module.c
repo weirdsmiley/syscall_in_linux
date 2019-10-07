105629cdea6b1ab1ac09343cdb8469f04953c3ee
Manas <manas18244@iiitd.ac.in>
2019-10-07 18:16:12 +0530

    debugged module

--- syscallmodule/sh_task_info_module.c
+++ syscallmodule/sh_task_info_module.c
@@ -2,21 +2,23 @@
 #include <linux/module.h>
 #include <linux/init.h>
 #include <linux/sched.h>
+#include <linux/sched/signal.h>
 
 MODULE_LICENSE("GPL");
 MODULE_AUTHOR("Neon");
 MODULE_DESCRIPTION("Fetch members of task_struct via pid_t of a process");
 
-static int __init sh_task_info_module_init(int pid){
+static int __init sh_task_info_module_init(void){
 	struct task_struct *task_list;
 
 	for_each_process(task_list){
 		printk(KERN_INFO "%s\n", task_list->comm);
 	}
-
+	
+	return 0;
 }
 
-static int __exit sh_task_info_module_exit(void){
+static void __exit sh_task_info_module_exit(void){
 	printk("exit\n");
 }
 
