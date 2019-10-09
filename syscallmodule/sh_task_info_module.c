1ec09560faf84ae63ab9529e9f12cd90c3893a90
Manas <manas18244@iiitd.ac.in>
2019-10-09 23:49:21 +0530

    comments added

--- syscallmodule/sh_task_info_module.c
+++ syscallmodule/sh_task_info_module.c
@@ -8,18 +8,38 @@ MODULE_LICENSE("GPL");
 MODULE_AUTHOR("Neon");
 MODULE_DESCRIPTION("Fetch members of task_struct via pid_t of a process");
 
-static int __init sh_task_info_module_init(void){
+static void printmembers(struct task_struct *);
+
+/* Wrapper function for printing the 
+ * members of task_struct
+ */
+static void printmembers(struct task_struct *task_list)
+{
+	printk(KERN_INFO "%s\n", task_list->comm);
+}
+
+
+/* For a pid, it traverses the task_list 
+ * and finds the appropriate task_struct,
+ * prints members in kernel log and also 
+ * in a file.
+ */
+static int __init sh_task_info_module_init(void)
+{
+
 	struct task_struct *task_list;
-	int pid = 4567;
+	int pid = 3321;
 	for_each_process(task_list){
-		if(task_list->pid == pid || task_list->pid == 2 || task_list->pid == 1)
-			printk(KERN_INFO "%s\n", task_list->comm);
+		if(task_list->pid == pid){
+			printmembers(task_list);
+		}
 	}
 	
 	return 0;
 }
 
-static void __exit sh_task_info_module_exit(void){
+static void __exit sh_task_info_module_exit(void)
+{
 	printk("exit\n");
 }
 
