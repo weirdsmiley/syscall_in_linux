8e02e84129201896ed9c7ae5242d9aec4c0c3d21
Manas <manas18244@iiitd.ac.in>
2019-10-25 12:49:57 +0530

    tmp commit

--- syscallmodule/sh_task_info_module.c
+++ syscallmodule/sh_task_info_module.c
@@ -16,18 +16,50 @@ MODULE_DESCRIPTION("Fetch members of task_struct via pid_t of a process");
 
 static void printmembers(struct task_struct *);
 static void saveinfile(struct task_struct *);
+static struct file *openfile();
+static void closefile(struct file *);
 
 /* Wrapper function for printing the 
  * members of task_struct
  */
-static void printmembers(struct task_struct *task)
-{
+static void printmembers(struct task_struct *task){
 	printk(KERN_INFO "Name\t:\t%s\n", task->comm);		// process name
 	printk(KERN_INFO "State\t:\t%ld\n", task->state);	// state
 }
 
-static void saveinfile(struct task_struct *task)
-{
+static struct file *openfile(){
+	struct file *file = NULL;
+	int err;
+	mm_segment_t prev_filesystem;
+
+	prev_filesystem = get_fs();
+	set_fs(get_ds());
+	file = filp_open("/home/neon/sh_task_info.txt", O_WRONLY, 0644);
+	set_fs(prev_filesystem);
+	
+	if(IS_ERR(file)){
+		err = PTR_ERR(file);
+		return NULL;
+	}
+	return file;
+}
+
+static void closefile(struct file *filetoclose){
+	filp_close(file, NULL);
+}
+
+static void saveinfile(struct task_struct *task){
+	mm_segment_t prev_filesystem;
+	prev_filesystem = get_fs();
+	set_fs(get_ds());
+
+	vfs_write(file, "hello", strlen("hello"), 0);
+
+	set_fs(prev_filesystem);
+}
+
+/*
+static void saveinfile(struct task_struct *task){
 	struct file *file;
 	mm_segment_t prev_filesystem = get_fs();
 	set_fs(get_ds());
@@ -37,15 +69,15 @@ static void saveinfile(struct task_struct *task)
 
 	if(IS_ERR(file)){
 		printk(KERN_ALERT "error: cannot open file\n");
-	} else{
+	} else {
 		mm_segment_t oldfs;
 		oldfs = get_fs();
 		set_fs(get_ds());
 
-		/*
-		prev_filesystem = get_fs();
-		set_fs(get_ds());
-		*/
+		
+		//prev_filesystem = get_fs();
+		//set_fs(get_ds());
+
 
 		char data[] = "feels";
 		//file = fget(filedesc);
@@ -53,18 +85,18 @@ static void saveinfile(struct task_struct *task)
 		// writing...
 		int ret = vfs_write(file, data, strlen(data), 0);
 		set_fs(oldfs);
-		/*
-		if(file){
-			vfs_write(file, data, strlen(data), 0);
-			fput(file);
-		}
-		*/
+	
+		//if(file){
+		//	vfs_write(file, data, strlen(data), 0);
+		//	fput(file);
+		//}
 
 		filp_close(file, NULL);
 		//sys_close(filedesc);
 	}
 	//set_fs(prev_filesystem);
 }
+*/
 
 
 /* For a pid, it traverses the task_list 
@@ -72,23 +104,21 @@ static void saveinfile(struct task_struct *task)
  * prints members in kernel log and also 
  * in a file.
  */
-static int __init sh_task_info_module_init(void)
-{
+static int __init sh_task_info_module_init(void){
 
 	struct task_struct *task;
 	int pid = 3321;
 	for_each_process(task){
 		if(task->pid == pid || task->pid == 2){
 			printmembers(task);
-			saveinfile(task);
+			//saveinfile(task);
 		}
 	}
 	
 	return 0;
 }
 
-static void __exit sh_task_info_module_exit(void)
-{
+static void __exit sh_task_info_module_exit(void){
 	printk("exit\n");
 }
 
